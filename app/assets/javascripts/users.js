803f812e4c9f0596f89b29f37f81db1be9415d75
Luke Steensen <luke.steensen@gmail.com>
2012-12-27 21:57:14 -0500

    actually disable form on submit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,27 +1,22 @@
 $(function(){
-	$('input:submit').bind('click', function(){
-		//console.log('submitting');
-		$('input:submit').attr('value','Submitting...');
-		//$('input:submit').attr('disabled', true);
-		$('.email-failure-message').text('');
-	});
-	$('.email-form form').bind('ajax:success', function(data, status, xhr){
-		//alert("success");
-		//console.log("success");
-		$('#user_email').css({border: '1px solid green'});
-		$('#user_email').remove();
-		$('input:submit').remove();
-		$('.email-failure-message').text('');
-		$('.email-form form').append('<p class="email-success-message">You are awesome. Scroll down to read more about Backpack.');
-	})
-	.bind('ajax:error', function(xhr, status, error){
-		//alert("error");
-		//console.log('error');
-		$('input:submit').removeAttr('disabled');
-		$('input:submit').attr('value','Request Invite');
-		$('#email-container').effect("shake", { times:3, distance:20 }, 500);
-		$('#user_email').css({border: '1px solid red'}).focus();
-		$('.email-failure-message').text('Sorry, that email has already been registered or is invalid.');
-	});
+
+  $('.email-form form').bind('ajax:success', function(data, status, xhr){
+    //alert("success");
+    //console.log("success");
+    $('#user_email').css({border: '1px solid green'});
+    $('#user_email').remove();
+    $('input:submit').remove();
+    $('.email-failure-message').text('');
+    $('.email-form form').append('<p class="email-success-message">You are awesome. Scroll down to read more about Backpack.');
+  })
+  .bind('ajax:error', function(xhr, status, error){
+    //alert("error");
+    //console.log('error');
+    $('input:submit').removeAttr('disabled');
+    $('input:submit').attr('value','Request Invite');
+    $('#email-container').effect("shake", { times:3, distance:20 }, 500);
+    $('#user_email').css({border: '1px solid red'}).focus();
+    $('.email-failure-message').text('Sorry, that email has already been registered or is invalid.');
+  });
 
 }); 
